bb357887371a65d4b937bfc54a86bbd80770eb10
doomsday9089371 <boelterwr@outlook.com>
2025-03-07 14:56:04 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function moveToClasses() {
+    window.location.href="classes.html";
+}
+
+function moveToBarbarian() {
+    window.location.href="barbarian.html";
+}
+
+function moveBackToClasses() {
+    window.location.href="classes.html";
+}
