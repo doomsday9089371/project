b4d72d9e161085b65ba4e35db0d405efb2307b9d
doomsday9089371 <boelterwr@outlook.com>
2025-03-12 12:25:38 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
+function home() {
+    window.location.href="homepage.html";
+}
 
+
+//move from home to classes
 function moveToClasses() {
     window.location.href="classes.html";
 }
 
+//submiting class
+function submitClass() {
+    window.location.href="test.html";
+}
+
+//move from classes to barbarian
 function moveToBarbarian() {
     window.location.href="barbarian.html";
 }
 
-function moveBackToClasses() {
+//move from classes to bard
+function moveToBard() {
+    window.location.href="bard.html";
+}
+
+//move from classes to cleric
+function moveToCleric() {
+    window.location.href="cleric.html";
+}
+
+//move from classes to druid
+function moveTodruid() {
+    window.location.href="druid.html";
+}
+
+//move from classes to fighter
+function moveToFighter() {
+    window.location.href="fighter.html";
+}
+
+//move from .... to classes
+function returnToClasses() {
     window.location.href="classes.html";
 }
