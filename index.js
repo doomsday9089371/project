a7d7105ad09fad5b37c34b135da5197f3a78fb34
doomsday9089371 <boelterwr@outlook.com>
2025-03-13 11:22:15 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function home() {
 //move from home to classes
 function moveToClasses() {
     window.location.href="classes.html";
+    console.log("Function: moveToClasses() Status: Working");
 }
 
 //submiting class
@@ -16,26 +17,31 @@ function submitClass() {
 //move from classes to barbarian
 function moveToBarbarian() {
     window.location.href="barbarian.html";
+    console.log("Function: moveToBarbarian() Status: Working");
 }
 
 //move from classes to bard
 function moveToBard() {
     window.location.href="bard.html";
+    console.log("Function: moveToBard() Status: working");
 }
 
 //move from classes to cleric
 function moveToCleric() {
     window.location.href="cleric.html";
+    console.log("Function: moveToCleric() Status: working");
 }
 
 //move from classes to druid
 function moveTodruid() {
     window.location.href="druid.html";
+    console.log("Function: moveTodruid() Status: working");
 }
 
 //move from classes to fighter
 function moveToFighter() {
     window.location.href="fighter.html";
+    console.log("Function: moveToFighter() Status: working")
 }
 
 //move from .... to classes
