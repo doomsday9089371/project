2fa7d58c037932d53c16f9fe5467d0d928ed9af6
doomsday9089371 <boelterwr@outlook.com>
2025-03-27 08:30:29 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,99 @@
+function home() {
+    window.location.href="homepage.html";
+}
 
+
+//move from home to classes
 function moveToClasses() {
     window.location.href="classes.html";
+    console.log("Function: moveToClasses() Status: Working");
 }
 
+function rollDice() {
+}
+
+function test() {
+    submitClass();
+    var testVar = "";
+    function myFunction() {
+        testVar = document,getElementById("testbutton").value;
+    }
+
+    function fillValue() {
+        document.getElementById("testInput").value = testVar;
+    }
+}
+
+//submiting class
+function submitClass() {
+    window.location.href="testpage.html";
+}
+
+//move to select barbarian class
 function moveToBarbarian() {
     window.location.href="barbarian.html";
+    console.log("Function: moveToBarbarian() Status: Working");
 }
 
-function moveBackToClasses() {
+//move to select barbarian sub-class
+function moveToPathOfTheZealot() {
+}
+
+//move from classes to bard
+function moveToBard() {
+    window.location.href="bard.html";
+    console.log("Function: moveToBard() Status: working");
+}
+
+//move from classes to cleric
+function moveToCleric() {
+    window.location.href="cleric.html";
+    console.log("Function: moveToCleric() Status: working");
+}
+
+//move from classes to druid
+function moveTodruid() {
+    window.location.href="druid.html";
+    console.log("Function: moveTodruid() Status: working");
+}
+
+//move from classes to fighter
+function moveToFighter() {
+    window.location.href="fighter.html";
+    console.log("Function: moveToFighter() Status: working");
+}
+
+function moveToMonk() {
+    window.location.href="monk.html";
+    console.log("Function: moveToMonk() Status: Working");
+}
+
+function moveToPaladin() {
+    window.location.href="paladin.html";
+    console.log("Function: moveToPaladin() Status: Working");
+}
+
+function moveToRanger() {
+    window.location.href="ranger.html";
+}
+
+function moveToRogue() {
+    window.location.href="rogue.html";
+}
+
+function moveToSorcerer() {
+    window.location.href="sorcerer.html";
+}
+
+function moveToWarlock() {
+    window.location.href="warlock.html";
+}
+
+function moveToWizard() {
+    window.location.href="wizard.html";
+}
+
+//move from .... to classes
+function returnToClasses() {
     window.location.href="classes.html";
 }
